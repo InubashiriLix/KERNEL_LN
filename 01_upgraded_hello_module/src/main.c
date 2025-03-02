85be3130cf167dbb8b74befb0b30b4a6db38c130
InubashriLix <lixinrong4002@gmail.com>
2025-03-02 19:21:12 +0800

    simply test

---
+++ 01_upgraded_hello_module/src/main.c
@@ -0,0 +1,19 @@
+
+#include <linux/init.h>
+#include <linux/module.h>
+
+static int __init my_init(void) {
+	printk(KERN_INFO "Hello, Kernel\n");
+	return 0;
+}
+
+static void __exit my_exit(void) {
+	printk(KERN_INFO "Goodbye, Kernel\n");
+}
+
+module_init(my_init);
+module_exit(my_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("InubashiriLix InubashiriLix@gmail.com");
+MODULE_DESCRIPTION("HELL THERE");
