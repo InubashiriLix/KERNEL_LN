647aa3af75843fdb50d0e4289edc658485c4b11d
InubashriLix <lixinrong4002@gmail.com>
2025-03-09 23:37:32 +0800

    kernel log leaerning

--- 04_kernel_log/src/main.c
+++ 04_kernel_log/src/main.c
@@ -2,21 +2,22 @@
 #include <linux/module.h>
 
 static int __init my_init(void) {
-// tset for kernel log levels
-printk(KERNEL_INFO "kernel info test\n");
-printk(KERNEL_WARNING "kernel wargning test\n");
-printk(KERNEL_CRIT "kernel crit test\n");
-printk(KERNEL_EMERG "kernel emerg test\n");
-printk(KERNEL_DEBUG "kernel debug test\n");
-printk(KERNEL_DEFAULT "kernel debug test\n");
+    // tset for kernel log levels
+    printk(KERN_INFO "kernel info test\n");
+    printk(KERN_WARNING "kernel wargning test\n");
+    printk(KERN_CRIT "kernel crit test\n");
+    printk(KERN_EMERG "kernel emerg test\n");
+    printk(KERN_DEBUG "kernel debug test\n");
+    printk(KERN_DEFAULT "kernel debug test\n");
     printk(KERN_INFO "Hello, Kernel\n");
     return 0;
 }
 
-static void __exit my_exit(void) { 
-	// kernel log functin test
-	
-	printk(KERN_INFO "Goodbye, Kernel\n"); }
+static void __exit my_exit(void) {
+    // kernel log functin test
+
+    printk(KERN_INFO "Goodbye, Kernel\n");
+}
 
 module_init(my_init);
 module_exit(my_exit);
